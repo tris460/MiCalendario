fcb05786d5186ec675b6a57e0b129085840f4e8c
Beatriz Martinez Perez <trism460@gmail.com>
2023-09-08 12:37:16 -0600

    Add initial design for calendar

--- src/app/pages/calendar/calendar.module.ts
+++ src/app/pages/calendar/calendar.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { FullCalendarModule } from '@fullcalendar/angular';
 import { IonicModule } from '@ionic/angular';
 
 import { CalendarPageRoutingModule } from './calendar-routing.module';
@@ -13,7 +14,8 @@ import { CalendarPage } from './calendar.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    CalendarPageRoutingModule
+    CalendarPageRoutingModule,
+    FullCalendarModule
   ],
   declarations: [CalendarPage]
 })
--- src/app/pages/calendar/calendar.page.ts
+++ src/app/pages/calendar/calendar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import esLocale from '@fullcalendar/core/locales/es';
 
 @Component({
   selector: 'app-calendar',
@@ -6,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.page.scss'],
 })
 export class CalendarPage implements OnInit {
-
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    events: [
+      {
+        title: '🤢🤧🥱',
+        start: '2023-09-10',
+        end: '2023-09-10',
+        allDay: true,
+        backgroundColor: '#fff',
+        borderColor: '#fff',
+      }
+    ],
+    locale: esLocale,
+  };
   constructor() { }
 
   ngOnInit() {
