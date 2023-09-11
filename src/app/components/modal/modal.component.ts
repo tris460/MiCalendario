8b8d6343ec5745c3de794ab12801e8f57b89130a
Beatriz Martinez Perez <trism460@gmail.com>
2023-09-11 14:52:07 -0600

    Add modal options

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -8,6 +8,11 @@ import { ModalController } from '@ionic/angular';
 })
 export class ModalComponent  implements OnInit {
   name: string = '';
+  emojisEmotions = ['🙂','🙃','😉','🫠','😊','😇','😀','😄','😁','😆','😅','😂','🥰','😍','🤩','😘','😗','😚','🥲','😋','😛','😜','🤪','🤑','🤗','🫢','🤭','🫣','🤫','🤔','🫡','🤐','🤨','😐','😑','😶','🫥','😏','😒','🙄','😬','😮‍💨','🤥','😌','😔','😪','🤤','😴','🤯','😵‍💫','🥳','🥸','🫤','😕','😟','🙁','😮','😳','🥺','🥹','😦','😨','😰','😢','😭','😱','😖','😓','😩','🥱','😤','😡','🤬','😠'];
+  emojisSymptoms = ['😷','😴','🤒','🤕','🤢','🤮','🤧','🥵','🥶','🥴','🤯','💩','👃','🧠','🫀','🫁','🦷','🦴'];
+
+  selectedEmojisEmotions: string[] = [];
+  selectedEmojisSymptoms: string[] = [];
 
   constructor(private modalCtrl: ModalController) {}
 
@@ -20,5 +25,18 @@ export class ModalComponent  implements OnInit {
 
   confirm() {
     return this.modalCtrl.dismiss(this.name, 'confirm');
+  }
+
+  toggleEmoji(emoji: string, selected: string[]) {
+    const index = selected.indexOf(emoji);
+    if (index === -1) {
+      selected.push(emoji);
+    } else {
+      selected.splice(index, 1);
+    }
+  }
+
+  isSelected(emoji: string, selected: string[]): boolean {
+    return selected.includes(emoji);
   }
 }
