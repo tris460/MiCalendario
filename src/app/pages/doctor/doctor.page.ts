46034895f07a55ae829f730b6d5167e0fc4586d2
Beatriz Martinez Perez <trism460@gmail.com>
2023-11-16 12:07:02 -0600

    Print doctor's name - Incomplete

--- src/app/pages/doctor/doctor.page.ts
+++ src/app/pages/doctor/doctor.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-doctor',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doctor.page.scss'],
 })
 export class DoctorPage implements OnInit {
+  doctors: any[] = [];
 
-  constructor() { }
+  constructor(private userService: UserService) {
+    this.userService.getUsers()
+      .then((res: any) => {
+        this.doctors = res.data.filter((user: any) => user.role === 'doctor');
+        console.log(this.doctors)
+      })
+      .catch((err: any) => {
+        console.log(err)
+      })
+  }
 
   ngOnInit() {
   }
