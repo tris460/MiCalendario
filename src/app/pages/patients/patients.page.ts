5d89a8891265316614a5625b0a5b2a79b594f2e6
Beatriz Martinez Perez <trism460@gmail.com>
2023-11-23 12:22:33 -0600

    Show appointments for doctor

--- src/app/pages/patients/patients.page.ts
+++ src/app/pages/patients/patients.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-patients',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patients.page.scss'],
 })
 export class PatientsPage implements OnInit {
+  userId: string | undefined;
+  isLoading: boolean = false;
+  appointments: string[] = [];
 
-  constructor() { }
+  constructor(private sharedService: SharedService, private userService: UserService) {
+    //Get the data of the current user
+    this.sharedService.loggedUser.subscribe((userData: any) => {
+      if (userData.data) {
+        this.userId = userData.data._id;
+      }
+    });
+  }
 
   ngOnInit() {
+    this.isLoading = true;
+    this.userService.getAppointments(this.userId!)
+    .then((res: any) => this.appointments = res.data)
+    .catch(err => {})
+    .finally(() => this.isLoading = false);
   }
 
 }
